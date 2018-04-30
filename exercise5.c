865d81d755c53f282c8f1ecf7ce56a7b0fc62c75
Henrique Costa <henrique@interacesso.pt>
2018-05-01 00:13:32 +0100

    Create exercise5.c

---
+++ exercise5.c
@@ -0,0 +1,10 @@
+/*
+ * Worksheet19_Exercise_5
+ * 
+ * Make a C program that uses only timer 0 square waves on pins P1.3, p1.4 and p1.5 pin with a frequency of 
+ * 1kHz, 10kHz and 500 Hz respectively. All waves should have a 50% duty-cycle. Execute the code on the board and confirm, 
+ * with the oscilloscope. Make use of the interrupt mechanism.
+ *
+ */
+
+#include <C8051F340.h>
