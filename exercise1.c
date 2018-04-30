105c51affca12a2bea634b597a3a3f4d5e372d34
Henrique Costa <henrique@interacesso.pt>
2018-05-01 00:03:50 +0100

    Create exercise1.c

---
+++ exercise1.c
@@ -0,0 +1,11 @@
+/*
+ * Worksheet19_Exercise1
+ * 
+ * Make a C program for the C8051F340 that creates a square wave on the P2.2  pin with a frequency of 2KHz. 
+ * This program should use the microcontroller interrupt system. Considering that the C8051F340 can use a 
+ * time of 1μS for each count of timer 0, what is the best mode of operation to generate this wave that 
+ * stays 250μs ON and another 250μs OFF. Simulate and test on the board.
+ *
+ */
+
+#include <C8051F340.h>
