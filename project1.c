fe6da991aeadfc203647b2dac612916be204abc7
Henrique Costa <henrique@interacesso.pt>
2018-05-01 00:14:41 +0100

    Create project1.c

---
+++ project1.c
@@ -0,0 +1,9 @@
+/*
+ * Worksheet19_Project_1
+ * 
+ * Create the a 1 KHz square wave with variable duty-cycle. The duty cycle should be defined by a threshold value that 
+ * is updated by two external interrupts. One interrupts increments the threshold, while the other should decrement it. 
+ *
+ */
+
+#include <C8051F340.h>
