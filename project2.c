71c30ade2aad4e3b81a64e74b9cc377f7acb2815
Henrique Costa <henrique@interacesso.pt>
2018-05-01 00:16:22 +0100

    Create project2.c

---
+++ project2.c
@@ -0,0 +1,8 @@
+/*
+ * Worksheet19_Project_2
+ * 
+ * [optional] Makes the necessary adjustments to previous project in a way that the threshold is precisely updated. 
+ *
+ */
+
+#include <C8051F340.h>
