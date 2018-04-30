314f85ec6c7121d94f0322d3fb929f8f41bdbae1
Henrique Costa <henrique@interacesso.pt>
2018-05-01 00:11:00 +0100

    Create exercise3.c

---
+++ exercise3.c
@@ -0,0 +1,10 @@
+/*
+ * Worksheet19_Exercise_3
+ * 
+ * Now do a program in C language that uses timer 1 to create a square wave on pin P2.2 with 1 millisecond ON 
+ * and 1 millisecond OFF. Run the program on the on-board and check with the oscilloscope the generated wave period. 
+ * What is now the best mode of operation to do this task with timer 1? This program should use the microcontroller interrupt system.
+ *
+ */
+
+#include <C8051F340.h>
