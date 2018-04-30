705ee8f753d9b28eacfd14560a602b0e4aadb02d
Henrique Costa <henrique@interacesso.pt>
2018-05-01 00:08:30 +0100

    Create exercise2.c

---
+++ exercise2.c
@@ -0,0 +1,12 @@
+*
+ * Worksheet19_Exercise_2
+ * 
+ * Create a project that changes the state of 8 leds depending on the state of the switches connected on pin P2.0. 
+ * Each time P2_0 is pressed', the LEDs should change their behavior from flash  to  maintain their state.
+ *     a) Configure the Simple Keypad and Led Panel virtual peripherals.
+ *     b) Simulate, step-by-step, while changing the state of the switches manually.
+ * This program should use the microcontroller external interrupt system. 
+ *
+ */
+
+#include <C8051F340.h>
