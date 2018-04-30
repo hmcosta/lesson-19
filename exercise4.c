87baab05b06a18d064bbbde82de0814e8eebe523
Henrique Costa <henrique@interacesso.pt>
2018-05-01 00:12:23 +0100

    Create exercise4.c

---
+++ exercise4.c
@@ -0,0 +1,10 @@
+/*
+ * Worksheet19_Exercise_4
+ * 
+ * Do the same exercise mentioned in exercise 1 but now using the timer 2 with the provided feature of auto-reload. 
+ * Run the program on the on-board and check with the oscilloscope the generated wave period. This program should use 
+ * the microcontroller interrupt system.
+ *
+ */
+
+#include <C8051F340.h>
